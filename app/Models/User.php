249f7857c0d8385c6b14c7c1aac9e54f5541c7b0
Di Depan Layar <albinmaulana@yahoo.com>
2025-11-20 12:15:33 +0700

    Feature: models and migration for Buyer

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Store::class);
     }
+
+    public function buyer()
+    {
+        return $this->hasOne(Buyer::class);
+    }
 }
