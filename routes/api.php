ea353f926048ba38f3d0aae05ef39b5cd7beb67b
Di Depan Layar <albinmaulana@yahoo.com>
2025-11-23 12:05:17 +0700

    Feature: API Store

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\StoreController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// User
 Route::apiResource('user', UserController::class);
 Route::get('user/all/paginated', [UserController::class, 'getAllPaginated']);
+
+# Store
+Route::apiResource('store', StoreController::class);
+Route::get('store/all/paginated', [StoreController::class, 'getAllPaginated']);
