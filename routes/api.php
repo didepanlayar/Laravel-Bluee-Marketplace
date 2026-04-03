4079eb3776138e153e0f2de3b02f215d07087505
Di Depan Layar <albinmaulana@yahoo.com>
2026-04-03 18:58:02 +0700

    Feature: API StoreBalanceHistory

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StoreBalanceController;
+use App\Http\Controllers\StoreBalanceHistoryController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,7 @@ Route::post('store/{id}/verified', [StoreController::class, 'updateVerifiedStatu
 // StoreBalance
 Route::apiResource('store-balance', StoreBalanceController::class)->except(['store', 'update', 'delete']);
 Route::get('store-balance/all/paginated', [StoreBalanceController::class, 'getAllPaginated']);
+
+// StoreBalanceHistory
+Route::apiResource('store-balance-history', StoreBalanceHistoryController::class)->except(['store', 'update', 'delete']);
+Route::get('store-balance-history/all/paginated', [StoreBalanceHistoryController::class, 'getAllPaginated']);
