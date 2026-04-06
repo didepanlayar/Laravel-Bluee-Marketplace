e5d1fb5a81aa3d3f4b4f49eca402f16802d12aa4
Di Depan Layar <albinmaulana@yahoo.com>
2026-04-06 11:19:53 +0700

    Feature: API Withdrawal

---
+++ app/Http/Resources/WithdrawalResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WithdrawalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'store_balance_id' => new StoreBalanceResource($this->storeBalance),
+            'amount' => (float) (string) $this->amount,
+            'bank_account_name' => $this->bank_account_name,
+            'bank_account_number' => $this->bank_account_number,
+            'bank_name' => $this->bank_name,
+            'status' => $this->status
+        ];
+    }
+}
