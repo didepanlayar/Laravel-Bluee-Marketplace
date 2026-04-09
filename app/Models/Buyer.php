ee9a6415b223fcef300a3673cd42dad3060445e7
Di Depan Layar <albinmaulana@yahoo.com>
2026-04-09 11:18:13 +0700

    Feature: API Buyer

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -15,6 +15,14 @@ class Buyer extends Model
         'phone_number'
     ];
 
+    // Scope Buyer Search
+    public function scopeSearch($query, $search)
+    {
+        return $query->whereHas('user', function ($q) use ($search) {
+            $q->where('name', 'like', '%' . $search . '%');
+        });
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
